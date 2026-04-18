50b57411e8aaee095f6c9cccdd3df09c9e62f181
FatimaTabassum743 <157966446+FatimaTabassum743@users.noreply.github.com>
2026-04-18 17:10:35 +0530

    Add files via upload

---
+++ guessing.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("Hello I am Omer")
+st.image("image.png",width=400)
+st.image("python.png",width=600)
+st.image("fruits.jpg",width=500)
+st.video("video.mp4")
+st.video("https://www.youtube.com/watch?v=7wnove7K-ZQ&list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg")
